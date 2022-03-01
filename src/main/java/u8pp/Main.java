20a3cf705d5ca1af19331ae10ce7d0b67431dc3c
Randy Ortiz <89422425+ZitroYdnar@users.noreply.github.com>
2022-03-01 05:09:38 +0000

    Flight and tic tac toe

--- src/main/java/u8pp/Main.java
+++ src/main/java/u8pp/Main.java
@@ -2,7 +2,16 @@ package u8pp;
 public class Main 
 {
     public static void main( String[] args ) {
-        System.out.println("Hi!\nIf you're seeing this message, you've successfully run the main method."
-        + "\nUse it to test your code during development.");
+        //System.out.println("Hi!\nIf you're seeing this message, you've successfully run the main method."
+       // + "\nUse it to test your code during development.");
+       
+        Flight testFlight = new Flight(2,2);
+
+        testFlight.reserveNextAvailableSeat("TestName1", true);
+        testFlight.reserveNextAvailableSeat("TestName2", true);
+        testFlight.reserveNextAvailableSeat("TestName3", false);
+
+        System.out.println(testFlight.getFrequentFlyers());
+    
     }
 }
--- src/main/java/u8pp/Reservation.java
+++ src/main/java/u8pp/Reservation.java
@@ -1,1 +1,30 @@
+
 package u8pp;
+
+public class Reservation{
+    // Instance Variables
+    private String name;
+    private boolean frequentFlyer;
+
+    //Constructors
+
+        
+        public Reservation(String tempName, boolean frequentFlyerY){
+            this.name = tempName;
+            this.frequentFlyer = frequentFlyerY;
+        }
+    //Acessor
+        public String getPassengerName(){
+            return this.name;
+        }
+
+        public boolean isFrequentFlyer(){
+            if(this.frequentFlyer == true){
+               return true; 
+            }
+            else{
+                return false;
+            }
+        }
+
+}
